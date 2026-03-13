51bdff464d74aba95e3053b8a06a52f6474cfcc4
tabeti <2313218194@qq.com>
2026-03-13 15:19:36 +0800

    搜索二维矩阵

--- src/main/java/mid/searchMatrix2.java
+++ src/main/java/mid/searchMatrix2.java
@@ -22,14 +22,15 @@ public class searchMatrix2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt(), n = sc.nextInt();
-        int[][] matrix = new int[m][n];
+        sc.nextLine();
+        int[][] nums = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                matrix[i][j] = sc.nextInt();
+                nums[i][j] = sc.nextInt();
             }
         }
         sc.nextLine();
         int target = sc.nextInt();
-        System.out.println(searchMatrix(matrix, target));
+        System.out.println(searchMatrix(nums, target));
     }
 }
