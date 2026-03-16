c24aa2f157bb729d17c3599c3d0b87ec1ec3ad38
tabeti <2313218194@qq.com>
2026-03-16 12:53:13 +0800

    有效的括号

--- src/main/java/easy/isValid.java
+++ src/main/java/easy/isValid.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 import java.util.Stack;
 
 /**
- * 括号匹配
+ * 有效的括号
  */
 public class isValid {
     private static boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
-        Stack<Character> st = new Stack<>();
+        Stack<Character> stack = new Stack<>();
         char[] ss = s.toCharArray();
-        for (int i = 0; i < ss.length; i++) {
-            if (ss[i] == '(') st.push(')');
-            else if (ss[i] == '[') st.push(']');
-            else if (ss[i] == '{') st.push('}');
-            else if (st.isEmpty() || st.peek() != ss[i]) return false;
-            else st.pop();
+        for (int i = 0; i < s.length(); i++) {
+            if (ss[i] == '(') stack.push(')');
+            else if (ss[i] == '[') stack.push(']');
+            else if (ss[i] == '{') stack.push('}');
+            else if (stack.isEmpty() || stack.peek() != ss[i]) return false;
+            else stack.pop();
         }
-        return st.isEmpty();
+        return stack.isEmpty();
     }
 
     public static void main(String[] args) {
