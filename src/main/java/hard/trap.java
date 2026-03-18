2c5a1d83562bee754de81cd03000bdaf0e72864d
tabeti <2313218194@qq.com>
2026-03-18 10:54:16 +0800

    接雨水

--- src/main/java/hard/trap.java
+++ src/main/java/hard/trap.java
@@ -8,24 +8,24 @@ import java.util.Stack;
  */
 public class trap {
     private static int trap(int[] height) {
-        Stack<Integer> st = new Stack<>();
-        st.push(0);
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
         int res = 0;
         for (int i = 1; i < height.length; i++) {
-            if (height[i] < height[st.peek()]) st.push(i);
-            else if (height[i] == height[st.peek()]) {
-                st.pop();
-                st.push(i);
+            if (height[i] < height[stack.peek()]) stack.push(i);
+            else if (height[i] == height[stack.peek()]) {
+                stack.pop();
+                stack.push(i);
             } else {
-                while (!st.isEmpty() && height[i] > height[st.peek()]) {
-                    int mid = st.pop();
-                    if (!st.isEmpty()) {
-                        int h = Math.min(height[st.peek()], i) - height[mid];
-                        int w = i - st.peek() - 1;
+                while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                    int mid = stack.pop();
+                    if (!stack.isEmpty()) {
+                        int h = Math.min(height[stack.peek()], height[i]) - height[mid];
+                        int w = i - stack.peek() - 1;
                         res += h * w;
                     }
                 }
-                st.push(i);
+                stack.push(i);
             }
         }
         return res;
@@ -33,12 +33,10 @@ public class trap {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        int[] height = new int[n];
-        String[] inputHeight = sc.nextLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            height[i] = Integer.parseInt(inputHeight[i]);
+        String[] split = sc.nextLine().split(" ");
+        int[] height = new int[split.length];
+        for (int i = 0; i < height.length; i++) {
+            height[i] = Integer.parseInt(split[i]);
         }
         System.out.println(trap(height));
     }
