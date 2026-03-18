5f084d04c72f2e77c8c5da4f37084627038b0eb5
tabeti <2313218194@qq.com>
2026-03-18 11:29:40 +0800

    寻找旋转排序数组中的最小值

--- src/main/java/mid/findMin.java
+++ src/main/java/mid/findMin.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class findMin {
     private static int findMin(int[] nums) {
         int n = nums.length;
-        int left = 0, right = nums.length - 1;
+        int left = 0, right = n - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[n - 1]) left = mid + 1;
