cf84aa7f63baa2c70884eb7b18b4331e1a8b55f3
tabeti <2313218194@qq.com>
2026-03-19 10:28:07 +0800

    最大子数组和

--- src/main/java/mid/maxSubArray.java
+++ src/main/java/mid/maxSubArray.java
@@ -10,23 +10,20 @@ public class maxSubArray {
     private static int maxSubArray(int[] nums) {
         int[] dp = new int[nums.length];
         dp[0] = nums[0];
-        int res = 0;
+        int res = nums[0];
         for (int i = 1; i < nums.length; i++) {
             dp[i] = Math.max(nums[i], dp[i - 1] + nums[i]);
-            //res = Math.max(res, dp[i]);
+            res = Math.max(res, dp[i]);
         }
-        System.out.println(Arrays.toString(dp));
         return res;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        int[] nums = new int[n];
-        String[] inputNums = sc.nextLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(inputNums[i]);
+        String[] split = sc.nextLine().split(" ");
+        int[] nums = new int[split.length];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(split[i]);
         }
         System.out.println(maxSubArray(nums));
     }
