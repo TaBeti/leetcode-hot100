8e5071478d368eeb295a4de3abb3d5ded4b71f9c
tabeti <2313218194@qq.com>
2026-03-19 11:18:49 +0800

    最小路径和

--- src/main/java/mid/minPathSum.java
+++ src/main/java/mid/minPathSum.java
@@ -23,7 +23,6 @@ public class minPathSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int m = sc.nextInt(), n = sc.nextInt();
-        sc.nextLine();
         int[][] grid = new int[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
