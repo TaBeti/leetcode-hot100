9ae77ce7d0122be233e93e106499b058183590a9
tabeti <2313218194@qq.com>
2026-03-23 10:58:20 +0800

    从前序与中序遍历序列构造二叉树

--- src/main/java/mid/buildTree.java
+++ src/main/java/mid/buildTree.java
@@ -20,23 +20,6 @@ public class buildTree {
         }
     }
 
-    private static TreeNode buildTree(int[] preorder, int[] inorder) {
-        if (preorder.length == 0 || inorder.length == 0) return null;
-        TreeNode root = new TreeNode(preorder[0]);
-        for (int i = 0; i < preorder.length; i++) {
-            if (preorder[0] == inorder[i]) {
-                int[] pre_left = Arrays.copyOfRange(preorder, 1, i + 1);
-                int[] pre_right = Arrays.copyOfRange(preorder, i + 1, preorder.length);
-                int[] in_left = Arrays.copyOfRange(inorder, 0, i);
-                int[] in_right = Arrays.copyOfRange(inorder, i + 1, inorder.length);
-                root.left = buildTree(pre_left, in_left);
-                root.right = buildTree(pre_right, in_right);
-                break;
-            }
-        }
-        return root;
-    }
-
     private static List<Integer> levelOrder(TreeNode root) {
         List<Integer> res = new ArrayList<>();
         if (root == null) return res;
@@ -52,6 +35,23 @@ public class buildTree {
             }
         }
         return res;
+    }
+
+    private static TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder.length == 0 || inorder.length == 0) return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        for (int i = 0; i < preorder.length; i++) {
+            if (preorder[0] == inorder[i]) {
+                int[] pre_left = Arrays.copyOfRange(preorder, 1, i + 1);
+                int[] pre_right = Arrays.copyOfRange(preorder, i + 1, preorder.length);
+                int[] in_left = Arrays.copyOfRange(inorder, 0, i);
+                int[] in_right = Arrays.copyOfRange(inorder, i + 1, inorder.length);
+                root.left = buildTree(pre_left, in_left);
+                root.right = buildTree(pre_right, in_right);
+                break;
+            }
+        }
+        return root;
     }
 
     public static void main(String[] args) {
