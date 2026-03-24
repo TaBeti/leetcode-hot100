e47ad1a0b9e9cb504179ab67a8b8445247217959
tabeti <2313218194@qq.com>
2026-03-24 11:25:55 +0800

    全排列

--- src/main/java/mid/permute.java
+++ src/main/java/mid/permute.java
@@ -11,25 +11,24 @@ public class permute {
     private static List<List<Integer>> res = new ArrayList<>();
     private static List<Integer> path = new ArrayList<>();
 
+    private static List<List<Integer>> permute(int[] nums) {
+        boolean[] used = new boolean[nums.length];
+        backtracking(nums, used);
+        return res;
+    }
     private static void backtracking(int[] nums, boolean[] used) {
         if (path.size() == nums.length) {
             res.add(new ArrayList<>(path));
             return;
         }
         for (int i = 0; i < nums.length; i++) {
-            if (used[i]) continue;
+            if (used[i] == true) continue;
             used[i] = true;
             path.add(nums[i]);
             backtracking(nums, used);
             path.remove(path.size() - 1);
             used[i] = false;
         }
-    }
-
-    private static List<List<Integer>> permute(int[] nums) {
-        boolean[] used = new boolean[nums.length];
-        backtracking(nums, used);
-        return res;
     }
 
     public static void main(String[] args) {
