b41689decab735b904f60875054b0dc4da112672
tabeti <2313218194@qq.com>
2026-03-24 11:47:10 +0800

    岛屿数量

--- src/main/java/mid/numIslands.java
+++ src/main/java/mid/numIslands.java
@@ -6,26 +6,14 @@ import java.util.Scanner;
  * 岛屿数量
  */
 public class numIslands {
-    private static int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
-    private static void dfs(char[][] grid, boolean[][] visited, int x, int y) {
-        for (int i = 0; i < 4; i++) {
-            int nextx = x + dir[i][0];
-            int nexty = y + dir[i][1];
-            if (nextx < 0 || nextx >= grid.length || nexty < 0 || nexty >= grid[0].length) continue;
-            if (!visited[nextx][nexty] && grid[nextx][nexty] == '1') {
-                visited[nextx][nexty] = true;
-                dfs(grid, visited, nextx, nexty);
-            }
-        }
-    }
+    private static int[][] dir = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
 
     private static int numIslands(char[][] grid) {
         int res = 0;
-        int m = grid.length, n = grid[0].length;
-        boolean[][] visited = new boolean[m][n];
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        int n = grid.length, m = grid[0].length;
+        boolean[][] visited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (!visited[i][j] && grid[i][j] == '1') {
                     res++;
                     visited[i][j] = true;
@@ -34,6 +22,19 @@ public class numIslands {
             }
         }
         return res;
+    }
+
+    private static void dfs(char[][] grid, boolean[][] visited, int x, int y) {
+        for (int i = 0; i < 4; i++) {
+            int nextx = x + dir[i][0];
+            int nexty = y + dir[i][1];
+            if (nextx < 0 || nextx >= grid.length || nexty < 0 || nexty >= grid[0].length)
+                continue;
+            if (!visited[nextx][nexty] && grid[nextx][nexty] == '1') {
+                visited[nextx][nexty] = true;
+                dfs(grid, visited, nextx, nexty);
+            }
+        }
     }
 
     public static void main(String[] args) {
