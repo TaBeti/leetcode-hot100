c30bbdb1ba9f244a41cb2b2b92eb8e6fe2d0fbd6
tabeti <2313218194@qq.com>
2026-03-27 15:36:49 +0800

    三数之和

--- src/main/java/mid/threeSum.java
+++ src/main/java/mid/threeSum.java
@@ -14,6 +14,7 @@ public class threeSum {
         List<List<Integer>> res = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) return res;
+            //对 a 去重
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             int left = i + 1, right = nums.length - 1;
             while (left < right) {
@@ -21,6 +22,7 @@ public class threeSum {
                 else if (nums[i] + nums[left] + nums[right] > 0) right--;
                 else {
                     res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    //对 b 和 c 去重
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
                     left++;
@@ -33,12 +35,10 @@ public class threeSum {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        int[] nums = new int[n];
-        String[] inputNums = sc.nextLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(inputNums[i]);
+        String[] split = sc.nextLine().split(" ");
+        int[] nums = new int[split.length];
+        for (int i = 0; i < split.length; i++) {
+            nums[i] = Integer.parseInt(split[i]);
         }
         System.out.println(threeSum(nums));
     }
