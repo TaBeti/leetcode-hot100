934a6d7540e6949f5f8af4ffcf92efa9d028c487
tabeti <2313218194@qq.com>
2026-03-27 16:17:07 +0800

    合并区间

--- src/main/java/mid/merge.java
+++ src/main/java/mid/merge.java
@@ -11,9 +11,9 @@ import java.util.Scanner;
 public class merge {
     private static int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
-        LinkedList<int[]> res = new LinkedList<>();
-        int left = intervals[0][0], right = intervals[0][1];
+        List<int[]> res = new LinkedList<>();
         res.add(intervals[0]);
+        int left = intervals[0][0], right = intervals[0][1];
         for (int i = 1; i < intervals.length; i++) {
             if (res.getLast()[1] >= intervals[i][0]) {
                 left = res.getLast()[0];
@@ -30,7 +30,6 @@ public class merge {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine();
         int[][] intervals = new int[n][2];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 2; j++) {
