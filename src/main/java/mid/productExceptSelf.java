517965787285d265fdd9905d4b26b0199dfe8ae3
tabeti <2313218194@qq.com>
2026-03-29 12:54:49 +0800

    除了自身以外数组的乘积

--- src/main/java/mid/productExceptSelf.java
+++ src/main/java/mid/productExceptSelf.java
@@ -4,28 +4,26 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * 除自身以外数组的乘积
+ * 除了自身以外数组的乘积
  */
 public class productExceptSelf {
     private static int[] productExceptSelf(int[] nums) {
         int n = nums.length;
-        int[] res = new int[n];
         int[] pre = new int[n], suf = new int[n];
         pre[0] = suf[n - 1] = 1;
         for (int i = 1; i < n; i++) pre[i] = pre[i - 1] * nums[i - 1];
         for (int i = n - 2; i >= 0; i--) suf[i] = suf[i + 1] * nums[i + 1];
+        int[] res = new int[n];
         for (int i = 0; i < n; i++) res[i] = pre[i] * suf[i];
         return res;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine();
-        int[] nums = new int[n];
-        String[] inputNums = sc.nextLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(inputNums[i]);
+        String[] split = sc.nextLine().split(" ");
+        int[] nums = new int[split.length];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = Integer.parseInt(split[i]);
         }
         System.out.println(Arrays.toString(productExceptSelf(nums)));
     }
