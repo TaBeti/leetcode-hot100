0622d54d68a90f1184d3597811d92e50580e82d2
tabeti <2313218194@qq.com>
2026-03-29 13:01:39 +0800

    旋转图像

--- src/main/java/mid/rotateImage.java
+++ src/main/java/mid/rotateImage.java
@@ -3,6 +3,9 @@ package mid;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * 旋转图像
+ */
 public class rotateImage {
     private static void rotate(int[][] matrix) {
         int n = matrix.length;
@@ -25,7 +28,6 @@ public class rotateImage {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine();
         int[][] matrix = new int[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
