975b23e18641d209783439a52aab630c07ba725b
tabeti <2313218194@qq.com>
2026-04-05 14:14:03 +0800

    最长递增子序列

--- src/main/java/mid/lengthOfLIS.java
+++ src/main/java/mid/lengthOfLIS.java
@@ -4,14 +4,14 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 /**
- * 最长递增子序列
+ * 最长递增子序列4.5
  */
 public class lengthOfLIS {
     private static int lengthOfLIS(int[] nums) {
         if (nums.length <= 1) return nums.length;
         int[] dp = new int[nums.length];
-        int res = 1;
         Arrays.fill(dp, 1);
+        int res = 1;
         for (int i = 1; i < nums.length; i++) {
             for (int j = 0; j < i; j++) {
                 if (nums[i] > nums[j]) dp[i] = Math.max(dp[i], dp[j] + 1);
