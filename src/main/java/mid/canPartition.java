1953f92224671b9ef7df7b75df98fbcefecb6e0d
tabeti <2313218194@qq.com>
2026-04-06 12:10:51 +0800

    分割等和子集

--- src/main/java/mid/canPartition.java
+++ src/main/java/mid/canPartition.java
@@ -10,15 +10,15 @@ public class canPartition {
         int sum = 0;
         for (int num : nums) sum += num;
         if (sum % 2 != 0) return false;
-        int target = sum / 2;
         int[] dp = new int[10001];
+        int target = sum / 2;
         for (int i = 0; i < nums.length; i++) {
             for (int j = target; j >= nums[i]; j--) {
                 dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
             }
             if (dp[target] == target) return true;
         }
-        return dp[target] == target;
+        return false;
     }
 
     public static void main(String[] args) {
